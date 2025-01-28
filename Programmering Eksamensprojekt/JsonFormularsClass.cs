772f55bd10d129a531c9fd3ba083ea81c2008db7
alexpallis <alex206f@gmail.com>
2025-01-28 16:03:11 +0100

    Json Files and stuff

---
+++ Programmering Eksamensprojekt/JsonFormularsClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Programmering_Eksamensprojekt
+{
+    public class Størrelse
+    {
+        public int Masse { get; set; }
+        public int Rumfang { get; set; }
+        public int Densitet { get; set; }
+        public int Tyngdeacceleration { get; set; }
+        public int Tyngdekraft { get; set; }
+    }
+
+    public class Formler
+    {
+        public List<int> Mvp { get; set; } // Corresponds to "m=v*p"
+        public List<int> Ftmg { get; set; } // Corresponds to "Ft=m*g"
+    }
+
+    public class Root
+    {
+        public Størrelse Størrelse { get; set; }
+        public Formler Formler { get; set; }
+    }
+
+}
--- Programmering Eksamensprojekt/Program.cs
+++ Programmering Eksamensprojekt/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Programmering_Eksamensprojekt;
+using System.Text.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,3 +10,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+class Testing
+{
+    static void Main(string[] args)
+    {
+        // Read JSON file
+        string json = File.ReadAllText("TestFile1.json");
+
+        Størrelse størrelse = JsonSerializer.Deserialize<Størrelse>(json);
+
+        Console.WriteLine($"Masse:{størrelse.Masse}");
+    }
+}
